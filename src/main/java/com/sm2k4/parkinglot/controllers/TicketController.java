0485b0852791c6a658113c1e13a09176cd1151c3
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-04-26 21:20:40 +0530

    creates controller,service and repository.

---
+++ src/main/java/com/sm2k4/parkinglot/controllers/TicketController.java
@@ -0,0 +1,10 @@
+package com.sm2k4.parkinglot.controllers;
+
+import com.sm2k4.parkinglot.dtos.IssueTicketRequestDto;
+import com.sm2k4.parkinglot.dtos.IssueTicketResponseDto;
+
+public class TicketController {
+    public IssueTicketResponseDto issueTicket(IssueTicketRequestDto requestDto){
+        return null;
+    }
+}
