6e4a1dd566053f4943495e008ebd6388a4be1449
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-04-26 20:10:29 +0530

    adds dtos and exception handling

---
+++ src/main/java/com/sm2k4/parkinglot/dtos/ResponseStatus.java
@@ -0,0 +1,6 @@
+package com.sm2k4.parkinglot.dtos;
+
+public enum ResponseStatus {
+    SUCCESS,
+    FAILURE,
+}
---
+++ src/main/java/com/sm2k4/parkinglot/exceptions/InvalidGateException.java
@@ -0,0 +1,7 @@
+package com.sm2k4.parkinglot.exceptions;
+
+public class InvalidGateException extends Exception{
+    public InvalidGateException(String message) {
+        super(message);
+    }
+}
